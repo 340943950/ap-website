af0bb9171f95b504b5b1f8e9fae59ded7c4bf343
Adarsh Padalia <Adarsh Padalia>
2024-09-23 10:35:25 -0400

    Personalized tab names and added icons to sidebar

--- app/pages.py
+++ app/pages.py
@@ -1,11 +1,31 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 
 bp = Blueprint("pages", __name__)
 
 @bp.route("/")
+def start():
+    return redirect("/home")
+
+@bp.route("/home")
 def home():
     return render_template("pages/home.html")
 
-@bp.route("/about")
-def about():
-    return render_template("pages/about.html")
+@bp.route("/projects")
+def projects():
+    return render_template("pages/projects.html")
+
+@bp.route("/experience")
+def experience():
+    return render_template("pages/experience.html")
+
+@bp.route("/skills")
+def skills():
+    return render_template("pages/skills.html")
+
+@bp.route("/resume")
+def resume():
+    return render_template("pages/resume.html")
+
+@bp.route("/contact")
+def contact():
+    return render_template("pages/contact.html")
